09121199c23b0ff347a36aae4b2539c2446d06f1
Sebbvw24 <s.delcanto@duocuc.cl>
2026-05-17 23:38:24 -0400

    Delete impexp directory

--- impexp/service_gestionusuarios/src/main/java/com/impexp/service_gestionusuarios/repository/GestionUsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.impexp.service_gestionusuarios.repository;
-
-import com.impexp.service_gestionusuarios.model.Gestionusuarios;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GestionUsuarioRepository extends JpaRepository<Gestionusuarios, Long> {
-}
--- impexp/service_usuario/src/main/java/com/impexp/service_usuario/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.impexp.service_usuario.repository;
-
-import com.impexp.service_usuario.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
